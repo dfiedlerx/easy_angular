82c9f37c789d9f045ff4f9852d45f57fccd6eed1
Daniel Fiedler <danielfiedler_@hotmail.com>
2022-04-18 11:43:05 -0300

    HomeTypeNewPost - Test Coverage

--- src/app/router-modules/home/home-type-new-post/home-type-new-post.component.spec.ts
+++ src/app/router-modules/home/home-type-new-post/home-type-new-post.component.spec.ts
@@ -1,14 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { postsConfigs } from 'src/app/shared/constants/configs/posts.configs';
 
 import { HomeTypeNewPostComponent } from './home-type-new-post.component';
 
 describe('HomeTypeNewPostComponent', () => {
   let component: HomeTypeNewPostComponent;
   let fixture: ComponentFixture<HomeTypeNewPostComponent>;
+  let exampleTyped = 'Something Typed';
+  let maxCharactersConfig = postsConfigs['maxCharacters'] as number;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeTypeNewPostComponent ]
+      declarations: [ HomeTypeNewPostComponent ],
+      imports: [ FormsModule ]
     })
     .compileComponents();
   });
@@ -16,10 +21,28 @@ describe('HomeTypeNewPostComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeTypeNewPostComponent);
     component = fixture.componentInstance;
+    component.typedMessage = exampleTyped;
+    component.typesLimit = maxCharactersConfig;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it ('should isInvalidCharactersLength returns false', () => {
+    expect(component.isInvalidCharactersLength()).toBeFalse();
+  });
+
+  it ('should isInvalidCharactersLength returns true', () => {
+    //A example value with a small number of characters
+    component.typesLimit = 5; 
+    expect(component.isInvalidCharactersLength()).toBeTrue();
+  });
+
+  it ('should getCharactersLeft returns the right value', () => {
+    expect(component.getCharactersLeft())
+      .toEqual(maxCharactersConfig - exampleTyped.length);
+  })
+
 });
