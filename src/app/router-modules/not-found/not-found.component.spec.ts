b47aece2f359b1616f1248c2137b7f1abce03183
Daniel Fiedler <danielfiedler_@hotmail.com>
2022-04-19 10:27:08 -0300

    Not FOund - Load Test Coverage

--- src/app/router-modules/not-found/not-found.component.spec.ts
+++ src/app/router-modules/not-found/not-found.component.spec.ts
@@ -1,25 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { NotFoundBackAreaStaticComponent } from './components/not-found-back-area-static/not-found-back-area-static.component'
+import { NotFoundTopAreaAnimatedComponent } from './components/not-found-top-area-animated/not-found-top-area-animated.component'
 
-import { NotFoundComponent } from './not-found.component';
+import { NotFoundComponent } from './not-found.component'
 
 describe('NotFoundComponent', () => {
-  let component: NotFoundComponent;
-  let fixture: ComponentFixture<NotFoundComponent>;
+  let component: NotFoundComponent
+  let fixture: ComponentFixture<NotFoundComponent>
+  let compiled: HTMLElement
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NotFoundComponent ]
-    })
-    .compileComponents();
-  });
+      declarations: [
+        NotFoundComponent,
+        NotFoundTopAreaAnimatedComponent,
+        NotFoundBackAreaStaticComponent,
+      ],
+    }).compileComponents()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NotFoundComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(NotFoundComponent)
+    component = fixture.componentInstance
+    compiled = fixture.debugElement.nativeElement
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  it('should load app-not-found-top-area-animated component', () => {
+    expect(
+      compiled.querySelector('app-not-found-top-area-animated'),
+    ).toBeInstanceOf(HTMLElement)
+  })
+
+  it('should load app-not-found-back-area-static component', () => {
+    expect(
+      compiled.querySelector('app-not-found-back-area-static'),
+    ).toBeInstanceOf(HTMLElement)
+  })
+})
