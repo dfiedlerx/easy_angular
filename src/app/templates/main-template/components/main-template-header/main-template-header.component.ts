cd7611db965d8db26b3e1d43fbb1b6e719211c05
Daniel Fiedler <danielfiedler_@hotmail.com>
2022-04-19 20:52:33 -0300

    Header Link with UserMock

--- src/app/templates/main-template/components/main-template-header/main-template-header.component.ts
+++ src/app/templates/main-template/components/main-template-header/main-template-header.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { homeRootRoute } from 'src/app/shared/constants/configs/routes.configs';
+import { SpecificUserDataOnInit } from 'src/app/shared/extends/specific-user-data-on-init/specific-user-data-on-init';
 @Component({
   selector: 'app-main-template-header',
   templateUrl: './main-template-header.component.html',
   styleUrls: ['./main-template-header.component.scss']
 })
-export class MainTemplateHeaderComponent implements OnInit {
+export class MainTemplateHeaderComponent extends SpecificUserDataOnInit {
 
   homeRootRoute : string = homeRootRoute;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  override ngOnInit(): void {
   }
 
 }
