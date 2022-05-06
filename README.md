536c2f04631549b694a096e6c696b323d0230e66
Daniel Fiedler <danielfiedler_@hotmail.com>
2022-05-06 16:26:45 -0300

    Change App Name - angular-projects-base-structutre

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TestSocialNetworkSpa
+# angular-projects-base-structutre
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.3.
 
--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/poc-dynamic-screen-spa'),
+      dir: require('path').join(__dirname, './coverage/angular-projects-base-structutre'),
       subdir: '.',
       reporters: [
         { type: 'html' },
