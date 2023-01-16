7d237dfd7009506056eb8ea04f28e25f369ea71b
Daniel Fiedler <32338795+dfiedlerx@users.noreply.github.com>
2023-01-16 09:46:10 -0300

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -45,7 +45,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
